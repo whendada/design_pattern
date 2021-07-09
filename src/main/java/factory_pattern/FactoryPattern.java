459ef39cac236db71ad94f31b329412604e2337a
wenkj <Dailian1234*+>
2021-07-09 15:51:57 +0800

    feature:factory_pattern

--- src/main/java/factory_pattern/FactoryPattern.java
+++ src/main/java/factory_pattern/FactoryPattern.java
@@ -1,5 +1,27 @@
 package factory_pattern;
 
 public class FactoryPattern {
-    
+
+    public static Shape getShape(String shapeType) {
+        if (null == shapeType) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Shape circle = FactoryPattern.getShape("circle");
+        circle.draw();
+        Shape rectangle = FactoryPattern.getShape("rectangle");
+        rectangle.draw();
+        Shape square = FactoryPattern.getShape("square");
+        square.draw();
+    }
 }
